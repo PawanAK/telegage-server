469800fc7a78fe3b5e1ba4bc06d5cc1c664d2c6c
Pawan Ajjar K <1nc20ci027@ncetmail.com>
2024-09-28 00:46:39 +0530

    add more_info

--- server.js
+++ server.js
@@ -130,7 +130,13 @@ app.post('/api/login', async (req, res) => {
       return res.status(400).json({ message: 'User not found or invalid credentials' });
     }
 
-    res.json({ message: 'Login successful', userId: user._id, has_community: user.has_community });
+    res.json({ 
+      message: 'Login successful', 
+      userId: user._id, 
+      username: user.username,
+      walletAddress: user.walletAddress,
+      has_community: user.has_community 
+    });
     console.log("Login successful", user);
   } catch (error) {
     res.status(500).json({ message: 'Error logging in', error: error.message });
